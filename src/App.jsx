ee62f913bd4d16be3f7e5cd8ee9dc2c811e94afd
Abhishek Pandey <abhishek.pandey998877@gmail.com>
2025-07-30 18:32:22 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+
+export default function App() {
+  return (
+    <div className="bg-gray-50 text-gray-900">
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
