0f58fc0ab39a7ef688a66c73dfddb52f44b1cb56
Abhishek Pandey <abhishek.pandey998877@gmail.com>
2025-08-04 16:44:32 +0530

    Added text by text motion in Hero sectio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react";
+
 const Hero = () => {
+  const text = "Crafting Web Solutions with cutting-edge technologies";
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      setDisplayedText(text.slice(0, index + 1));
+      index++;
+      if (index === text.length) {
+        clearInterval(typingInterval);
+      }
+    }, 50);
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <section
       id="hero"
@@ -7,9 +25,7 @@ const Hero = () => {
       <h1 className="text-5xl md:text-7xl lg:8xl font-bold max-w-full">
         FullStack Developer
       </h1>
-      <p className="text-lg md:text-2xl mt-4 ">
-        Crafting Web Solutions with cutting-edge technologies
-      </p>
+      <p className="text-lg md:text-2xl mt-4 ">{displayedText}</p>
       <div className="mt-10 ">
         <a
           href="#contact"
